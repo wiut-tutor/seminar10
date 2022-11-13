787ffd181d1ad899bfef10b4e7a9a3959dc0ee95
wiut-tutor <wiut.tutor@gmail.com>
2022-11-13 19:58:08 +0500

    procedural programming

---
+++ external.py
@@ -0,0 +1,4 @@
+def display_average(mark):
+    print(mark)
+    #you can try to modify it further to have other functions like take input from user or display it
+    #in more pleasant form or have a range of other useful functions
--- test.py
+++
@@ -1,3 +0,0 @@
-from Student import Student
-student3 = Student('Adam', 'Smith', 1111, {'CSF': 75, 'FunPro': 80, 'WT': 85})
-student3.display_average()
